09c5a8ca70da6483c2470f2f704577cd2fccb1f2
Max Qiu <maxzqiu@gmail.com>
2024-03-10 11:02:35 -0700

    v1

--- src.js
+++ src.js
@@ -1,12 +1,15 @@
-const N=5;
 let TIME_LIMIT=30
-const URL = "https://opentdb.com/api.php?amount="+N+"&type=multiple"
+const URL = "https://opentdb.com/api.php?"
 const QUESTION = document.querySelector("#question")
 const BUTTON1 = document.querySelector("#choice-1")
 const BUTTON2 = document.querySelector("#choice-2")
 const BUTTON3 = document.querySelector("#choice-3")
 const BUTTON4 = document.querySelector("#choice-4")
 const TIMER = document.querySelector("#timer")
+const QUIZ_OPTIONS = document.querySelector("#quiz-options-container")
+const DIFFICULTY = document.querySelector("#difficulty")
+const START = document.querySelector("#start")
+const NUMBER = document.querySelector("#num-problems")
 
 const BUTTONS = [BUTTON1, BUTTON2, BUTTON3, BUTTON4]
 const QUIZ_CONTAINER = document.querySelector("#quiz-container")
@@ -52,10 +55,9 @@ function updateQuestions(data){
 }
 
 function displayScore(data,selected_choices){
-    let score=0
-    
-        
-    for (let i=0;i<N;i+=1){
+    let score=0      
+    let len=selected_choices.length  
+    for (let i=0;i<len;i+=1){
         let answer=data["results"][i]["correct_answer"]
         if (answer===selected_choices[i]){
             score+=1
@@ -63,7 +65,7 @@ function displayScore(data,selected_choices){
     }
     QUIZ_CONTAINER.replaceChildren();
     let score_display=document.createElement("p")
-    score_display.textContent="Your Score: "+ score.toString()
+    score_display.textContent="Your Score: "+ score.toString() + " / " + len.toString();
     QUIZ_CONTAINER.appendChild(score_display)
     console.log(selected_choices)
 }
@@ -87,8 +89,14 @@ function start_countdown(time,data,selected_choices){
 }
 
 
-async function getQuestion() {
-    let res = await fetch(URL, { mode: 'cors', method: 'GET' })
+async function getQuestion(amount, difficulty) {
+    let query = new URLSearchParams({
+        type:"multiple",
+        amount:amount,
+        difficulty:difficulty,
+    });
+
+    let res = await fetch(URL+query, { mode: 'cors', method: 'GET' })
     let data = await res.json();
     let question_number=0
     let selected_choices=[];
@@ -100,7 +108,7 @@ async function getQuestion() {
         button.onclick = () => {
             question_number+=1
             selected_choices.push(button.textContent);
-            if (question_number===N){
+            if (question_number===amount){
                 displayScore(data,selected_choices);
             } else{
                 updateQuestions(data["results"][question_number])
@@ -111,4 +119,12 @@ async function getQuestion() {
     
 }
 
-getQuestion()
+START.onclick = () => {
+    if (isNaN(NUMBER.value)===true || NUMBER.value<1 || NUMBER.value>50){
+        alert("Input is not valid. ")
+        return;
+    }
+    getQuestion(NUMBER.value, DIFFICULTY.value)
+    QUIZ_CONTAINER.classList.remove("hidden")
+    QUIZ_OPTIONS.classList.add("hidden")
+}
